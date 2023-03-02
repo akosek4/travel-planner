df25f51c2f2e07a30bf6c7490503889ee8b16ee9
alysonkosek <akosek4@gmail.com>
2023-03-02 12:06:50 -0800

    Phase 2: reader and writer in progress: todo=add activities and fix static issue

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,8 @@ package ui;
 //Represents the main class
 
 public class Main {
+
+    //EFFECTS: creates a new list of trip application with an empty list of trips
     public static void main(String[] args) {
         new ListOfTripApp();
     }
